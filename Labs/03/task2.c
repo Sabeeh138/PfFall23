3f3854f601699eeab33190761911027356481615
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-05 10:37:30 +0500

    Create task2.c

---
+++ Labs/03/task2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+	float p = 0.0, b = 0.0, h = 0.0;
+	printf("Enter Length of perpendicular side: ");
+	scanf("%f" ,&p);
+	printf("Enter Length of base: ");	
+	scanf("%f" ,&b);
+	float ans = (p*p) + (b*b);
+	h = sqrt(ans);
+	printf(" Hypotenuse is: %f" , h);
+	return 1;
+}//end main() 
