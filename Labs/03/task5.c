c8a69638c4fd1c7dd6d8f2f52907eebda1c1a9e2
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-10 01:36:47 +0500

    Create task5.c

---
+++ Labs/03/task5.c
@@ -0,0 +1,13 @@
+int main() {
+    
+    int south1 = 10;
+    int west = 10;
+    int south2 = 5;
+    int north = 15;
+    double fuelConsumptionRate = 2.0; // 2 liters per kilometer
+    int totalDistance = south1 + south2 + north + west; 
+    double fuelConsumed = totalDistance * fuelConsumptionRate;
+    printf("Total distance covered: %d kilometers\n", totalDistance);
+    printf("Fuel consumed: %.2f liters\n", fuelConsumed);
+    return 1;
+}//endmain() 
