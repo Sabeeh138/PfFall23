e4b9e2d8fe101dbb6da917789b63b6f27ae6a27e
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-10 01:52:36 +0500

    Update task4.c

--- Labs/03/task4.c
+++ Labs/03/task4.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
-
-int main() {
-    char operation;
-    double n1, n2;
-
-    printf("Enter an operator (+, -, *, /): ");
-    scanf("%c", &operation);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&n1, &n2);
-
-    switch(operation)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",n1, n2, n1+n2);
-            break;
-
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",n1, n2, n1-n2);
-            break;
-
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf",n1, n2, n1*n2);
-            break;
-
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",n1, n2, n1/n2);
-            break;
-
-        default:
-            printf("Error! operator is not correct");
-    }
-
-    return 0;
+int main(){
+   char Operator;
+   float num1, num2, result = 0;
+   printf(" Enter any one operator like +, -, *, / : ");
+   scanf("%c", &Operator);
+   printf("Enter the values of Operands num1 and num2 ");
+   scanf("%f%f", &num1, &num2);
+   switch(Operator){
+      case '+': result = num1 + num2;
+         break;
+      case '-': result = num1 - num2;
+         break;
+      case '*': result = num1 * num2;
+         break;
+      case '/': result = num1 / num2;
+         break;
+      default: printf("Invalid Operator ");
+   }
+   printf("The value = %f", result);
+   return 0;
 }//end main()
