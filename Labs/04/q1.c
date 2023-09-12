187d152d3f4e1c479b9162fb33319c52abf13d53
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-12 10:04:12 +0500

    Update q1.c

--- Labs/04/q1.c
+++ Labs/04/q1.c
@@ -1,15 +1,26 @@
-// Online C compiler to run C program online
+/*
+*Programmer: Muhammad Sabeeh
+*Data: (9/12/2023)
+*Description: Print pattern for any Number n
+*/
+
+//--Included files--//
 #include <stdio.h>
 
 int main(){
 	int n, i, c;
+	i = 0, c = 0;
 	printf("Enter a Number 'n': ");
 	scanf("%d", &n);
-	for ( i = 0 ; i < n; i++ ) {
-	for(c = 0; c <= i; c++){
+
+	while ( i < n ) {
+	c = 0; 
+	while (c<= i) {
 		printf("*");
-	}	
+	c++;
+}	
 	printf("\n"); // new line
+	i++;
   }
 	return 1;
 }
