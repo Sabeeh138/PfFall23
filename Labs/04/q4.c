48b5aab7542d54e98ec5af5d4dc56ba006181d52
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-12 19:30:07 +0500

    Create q4.c

---
+++ Labs/04/q4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+main(){
+	int a,b,c,z;
+	float dis,x,y;
+	printf("enter the value of a \n");
+	scanf("%d", &a);
+
+	printf("enter the value of b \n");
+	scanf("%d", &b);
+
+	printf("enter the value of c \n");
+	scanf("%d", &c);
+
+	z=(b*b)-(4*a*c);
+	dis=sqrt(z);
+	x=(-b+dis)/(2*a);
+	y=(-b-dis)/(2*a);
+	printf("the 2 solutions are x1 %f and x2 %f",x,y);
+}
