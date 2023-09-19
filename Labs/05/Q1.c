23975e219e3926646f4686e25994967f787f59c1
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-09-19 08:54:01 +0500

    Create Q1.c

---
+++ Labs/05/Q1.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# include<math.h>
+	main(){
+	int num;
+	printf("Enter Num:");
+	scanf("%d", &num);
+	if (num%3 == 0){
+	printf("Num is a multiple of 3");
+	}
+	else {
+	printf("Num is not a multiple of 3");
+}
+}
