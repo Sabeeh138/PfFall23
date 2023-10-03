e721e314be3ac524a19fe7608ba7319fe911cd27
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-03 10:11:22 +0500

    Update Q3.c

--- Labs/06/Q3.c
+++ Labs/06/Q3.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    int n1, n2, LCM = 1, HCF = 1, f = 2;
+
+    printf("Enter num1: ");
+    scanf("%d", &n1);
+
+    printf("Enter num2: ");
+    scanf("%d", &n2);
+
+    while (f <= n1 && f <= n2) {
+        if (n1 % f == 0 && n2 % f == 0) {
+            HCF = HCF * f;
+            n1 = n1 / f;
+            n2 = n2 / f;
+        } else {
+            f++;
+        }
+    }
+    LCM = (n1 * n2 * HCF);
+
+    printf("HCF: %d\n", HCF);
+    printf("LCM: %d\n", LCM);
+
+    return 0;
