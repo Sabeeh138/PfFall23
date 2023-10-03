d1df2edb2149f069569aae2e788e7380e4fe0a97
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-03 09:07:23 +0500

    Create Q1.c

---
+++ Labs/06/Q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, count = 1;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (count < num) {
+        if (num % count == 0) {
+            sum += count;
+        }
+        count++;
+    }
+
+    if (sum == num) {
+        printf("Number is a perfect number\n");
+    } else {
+        printf("Not a perfect number\n");
+    }
+
+    return 0;
+}
