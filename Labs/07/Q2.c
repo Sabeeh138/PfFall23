a1d12a532128f72919156278a3e4bbb0f8a22897
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-10 09:44:19 +0500

    Create Q2.c

---
+++ Labs/07/Q2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+void rotateLeft(int arr[], int N, int d) {
+    d = d % N;
+
+    if (d < 0) {
+        d = N + d;
+    }
+
+    int cycles = gcd(N, d);
+
+    for (int i = 0; i < cycles; i++) {
+        int temp = arr[i];
+        int j = i;
+
+        while (1) {
+            int k = (j + d) % N;
+
+            if (k == i) {
+                break;
+            }
+
+            arr[j] = arr[k];
+            j = k;
+        }
+
+        arr[j] = temp;
+    }
+}
+
+int main() {
+    int N, d;
+    printf("Enter the size of the array: ");
+    scanf("%d", &N);
+
+    int arr[N];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number of positions to rotate left: ");
+    scanf("%d", &d);
+
+    rotateLeft(arr, N, d);
+
+    printf("Rotated Array: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
