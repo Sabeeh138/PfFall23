e005e8dcf4a5561436e0e0860b261d106a57746d
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-10 08:47:29 +0500

    Create Q3.c

---
+++ Labs/07/Q3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int number, originalNumber, remainder, sum = 0;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    originalNumber = number; 
+
+    while (number != 0) {
+        remainder = number % 10;  
+        sum += remainder;        
+        number /= 10;            
+    }
+
+    printf("The sum of the digits of %d is %d\n", originalNumber, sum);
+
+    return 0;
+}
