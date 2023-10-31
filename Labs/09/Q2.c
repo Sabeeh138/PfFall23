5300d94fbc21dcf4196f39b0ccc6491174b75d45
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-31 08:46:37 +0500

    Create Q2.c

---
+++ Labs/09/Q2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void reverse(int *arr, int size) {
+    int *start = arr;
+    int *end = arr + size - 1;
+    int temp;
+
+    while (start < end) {
+        // Swap the elements at 'start' and 'end'
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        // Move the pointers inward
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[10];
+    int i;
+
+    printf("Enter 10 integers:\n");
+
+    // Input values from the user
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    // Call the reverse function
+    reverse(arr, 10);
+    printf("\nreversed array\n:");
+    for (i = 0; i < 10; i++) {
+        printf(" %d : %d\n ",i+1, arr[i]);
+    }
+
+    return 0;
+}
