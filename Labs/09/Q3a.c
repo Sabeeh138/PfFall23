8fbe88c22e0574e372ab9383c4d0bec49fbfae8a
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-10-31 09:35:26 +0500

    Update Q3a.c

--- Labs/09/Q3a.c
+++ Labs/09/Q3a.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 int main() {
-    int N;
+    int N,i;
     printf("Enter the size of the arrays: ");
     scanf("%d", &N);
 
@@ -12,13 +12,13 @@ int main() {
 
     // Create and initialize arrays
     char Arrayx[N];
-    int  Arrayy[N];
+    int Arrayy[N];
     long long int Arrayz[N];
 
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         Arrayx[i] = rand() % 256;  // Random char value between 0 and 255
         Arrayy[i] = rand();
-        Arrayz[i] = ((long long int)rand() << 32) | rand();
+        Arrayz[i] = ((long long int)rand() << 32) | (long long int)rand(); // Corrected this line
     }
 
     // Print addresses and values using pointer arithmetic
@@ -27,23 +27,22 @@ int main() {
     long long int *longLongPtr = Arrayz;
 
     printf("\nPrinting Arrayx:\n");
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         printf("Address: %p, Value: %d\n", (void *)charPtr, *charPtr);
         charPtr++;
     }
 
     printf("\nPrinting Arrayy:\n");
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         printf("Address: %p, Value: %d\n", (void *)intPtr, *intPtr);
         intPtr++;
     }
 
     printf("\nPrinting Arrayz:\n");
-    for (int i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         printf("Address: %p, Value: %lld\n", (void *)longLongPtr, *longLongPtr);
         longLongPtr++;
     }
 
     return 0;
 }
-//took help from internet and muhammad abbas
