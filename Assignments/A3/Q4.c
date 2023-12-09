f30f5701228eff15a566906f3a2d639203afd343
Sabeeh138 <142867481+Sabeeh138@users.noreply.github.com>
2023-12-09 15:04:41 +0500

    Update Q4.c

--- Assignments/A3/Q4.c
+++ Assignments/A3/Q4.c
@@ -3,8 +3,10 @@ programmer:Muhammad Sabeeh
 ID:7/12/2023
 Descr: a program that calculates highest salary paid to workers in each department annd the detail of those worker
              and the totol salaries paid by each department
-date:7/12/23
 */
+#include <stdio.h>
+#include <string.h>
+
 #include <stdio.h>
 #include <string.h>
 
@@ -18,55 +20,78 @@ struct Employee {
     char department[20];
 };
 
-// Function to find workers with the highest salary in each department
-void findMaxSalaryEmployees(struct Employee employees[], int size) {
-    char uniqueDepartments[10][20];  // Array to store unique department names
-    int maxSalaries[10] = {0};       // Array to store maximum salary for each department
+// Function to display workers with the maximum salary for each department
+void displayMaxSalaryEmployees(struct Employee employees[], int size) {
+    char departments[10][20];
+    int maxSalaries[10] = {0};
+    int maxSalaryIndexes[10] = {-1};  
 
     // Initialize arrays with empty strings
     for (int i = 0; i < 10; i++) {
-        strcpy(uniqueDepartments[i], "");
+        strcpy(departments[i], "");
     }
 
-    // Find maximum salary for each department
+    // Find maximum salary and index for each department
     for (int i = 0; i < size; i++) {
-        if (employees[i].salary > maxSalaries[i]) {
-            maxSalaries[i] = employees[i].salary;
-            strcpy(uniqueDepartments[i], employees[i].department);
+        int departmentIndex = -1;
+
+        // Find or create department entry
+        for (int j = 0; j < 10; j++) {
+            if (strcmp(employees[i].department, departments[j]) == 0) {
+                departmentIndex = j;
+                break;
+            } else if (strcmp(departments[j], "") == 0) {
+                strcpy(departments[j], employees[i].department);
+                departmentIndex = j;
+                break;
+            }
+        }
+
+        if (departmentIndex != -1) {
+            // Update max salary and index if the current salary is higher
+            if (employees[i].salary > maxSalaries[departmentIndex]) {
+                maxSalaries[departmentIndex] = employees[i].salary;
+                maxSalaryIndexes[departmentIndex] = i;
+            }
         }
     }
 
-    // Display details of workers with maximum salary for each department
-    printf("Details of Employees with Highest Salary in Each Department:\n");
+    // Display details of employees with maximum salary for each department
+    printf("Details of Employees with Maximum Salary for Each Department:\n");
     for (int i = 0; i < 10; i++) {
-        if (strcmp(uniqueDepartments[i], "") != 0) {
-            printf("Department: %s\n", uniqueDepartments[i]);
-            printf("Employee ID: %d\n", employees[i].employeeId);
-            printf("Name: %s %s\n", employees[i].firstName, employees[i].lastName);
-            printf("Salary: %d\n", maxSalaries[i]);
-            printf("Joining Date: %s\n", employees[i].joiningDate);
-            printf("-------------\n");
+        if (strcmp(departments[i], "") != 0) {
+            // Print details only if there is an employee with the max salary in the department
+            if (maxSalaryIndexes[i] != -1) {
+                printf("Department: %s\n", departments[i]);
+                printf("Employee ID: %d\n", employees[maxSalaryIndexes[i]].employeeId);
+                printf("Name: %s %s\n", employees[maxSalaryIndexes[i]].firstName, employees[maxSalaryIndexes[i]].lastName);
+                printf("Salary: %d\n", maxSalaries[i]);
+                printf("Joining Date: %s\n", employees[maxSalaryIndexes[i]].joiningDate);
+                printf("-------------\n");
+            }
         }
     }
 }
 
-// Function to calculate total salaries paid by each department
-void calculateTotalSalariesByDepartment(struct Employee employees[], int size) {
-    char uniqueDepartments[10][20]; // Array to store unique department names
-    int totalSalaries[10] = {0};    // Array to store total salary for each department
+// Function to calculate and display total salaries paid by each department
+void fetchTotalSalariesByDepartment(struct Employee employees[], int size) {
+    char departments[10][20];
+    int totalSalaries[10] = {0};
+    int i, j;
 
-    for (int i = 0; i < 10; i++) {
-        strcpy(uniqueDepartments[i], "");
+    // Initialize arrays with empty strings
+    for (i = 0; i < 10; i++) {
+        strcpy(departments[i], "");
     }
 
     // Calculate total salary for each department
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < 10; j++) {
-            if (strcmp(employees[i].department, uniqueDepartments[j]) == 0) {
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < 10; j++) {
+            if (strcmp(employees[i].department, departments[j]) == 0) {
                 totalSalaries[j] += employees[i].salary;
                 break;
-            } else if (strcmp(uniqueDepartments[j], "") == 0) {
-                strcpy(uniqueDepartments[j], employees[i].department);
+            } else if (strcmp(departments[j], "") == 0) {
+                strcpy(departments[j], employees[i].department);
                 totalSalaries[j] += employees[i].salary;
                 break;
             }
@@ -75,17 +100,16 @@ void calculateTotalSalariesByDepartment(struct Employee employees[], int size) {
 
     // Display departments along with total salaries
     printf("\nDepartments and Total Salaries:\n");
-    for (int i = 0; i < 10; i++) {
-        if (strcmp(uniqueDepartments[i], "") != 0) {
-            printf("%s - %d\n", uniqueDepartments[i], totalSalaries[i]);
+    for (i = 0; i < 10; i++) {
+        if (strcmp(departments[i], "") != 0) {
+            printf("%s - %d\n", departments[i], totalSalaries[i]);
         }
     }
 }
 
 int main() {
-    printf("Muhammad Sabeeh\n23k-0002\n\n\n");
+    printf("Muhammad Sabeeh\n23k-0002\n\n");
 
-    // Initializing an array of structures for employee data
     struct Employee employees[] = {
         {1, "Monika", "Arora", 100000, "2014-02-20 09:00:00", "HR"},
         {2, "Niharika", "Verma", 80000, "2014-06-11 09:00:00", "Admin"},
@@ -97,11 +121,11 @@ int main() {
         {8, "Geetika", "Chauhan", 90000, "2014-04-11 09:00:00", "Admin"},
     };
 
-    // Calculate the number of elements in the employees array
     int size = sizeof(employees) / sizeof(employees[0]);
 
-    findMaxSalaryEmployees(employees, size);
-    calculateTotalSalariesByDepartment(employees, size);
+    displayMaxSalaryEmployees(employees, size);
+
+    fetchTotalSalariesByDepartment(employees, size);
 
     return 0;
 }
